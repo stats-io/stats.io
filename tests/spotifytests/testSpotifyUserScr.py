6d8fb9d61fb2feb7351f394bf8d644c332cee043
Grzegorz Dynak <grzegorzdynaq@gmail.com>
2023-04-26 22:42:40 +0200

    Turning the test into 3 smaller ones and add new test with sample data

--- tests/spotifytests/testSpotifyUserScr.py
+++ tests/spotifytests/testSpotifyUserScr.py
@@ -1,18 +1,27 @@
 import unittest
 from aplication.app import SpotifyUserScreen, setUp
+import pandas as pd
 
 
 class TestSpotifyUserScreen(unittest.TestCase):
     app = setUp()
     app.screen_manager = app.build()
 
-    def test_change_screen(self):
+    def test_change_screen_to_spotify_top_list_screen(self):
         spotify_user_screen = SpotifyUserScreen()
         self.app.screen_manager.current = "Spotify_Main_Screen"
         spotify_user_screen.changeScreen("Spotify_Top_List_Screen")
         self.assertEqual(self.app.screen_manager.current, "Spotify_Top_List_Screen")
+
+    def test_change_screen_to_spotify_history_screen(self):
+        spotify_user_screen = SpotifyUserScreen()
+        self.app.screen_manager.current = "Spotify_Main_Screen"
         spotify_user_screen.changeScreen("Spotify_History_Screen")
         self.assertEqual(self.app.screen_manager.current, "Spotify_History_Screen")
+
+    def test_change_screen_to_spotify_main_screen(self):
+        spotify_user_screen = SpotifyUserScreen()
+        self.app.screen_manager.current = "Spotify_History_Screen"
         spotify_user_screen.changeScreen("Spotify_Main_Screen")
         self.assertEqual(self.app.screen_manager.current, "Spotify_Main_Screen")
 
@@ -27,3 +36,15 @@ class TestSpotifyUserScreen(unittest.TestCase):
 
         self.assertIsNotNone(spotify_user_screen.spotifyData)
         self.assertIsNotNone(spotify_user_screen.historyData)
+
+    def test_hitory_sample_data(self):
+        spotify_user_screen = SpotifyUserScreen()
+
+        with open("./sampledata/spotify_history.csv") as file:
+            reader = pd.read_csv(file)
+            data = reader.values.tolist()
+
+        history = spotify_user_screen.historyData
+        iter_data = iter(data)
+        for i, j in zip(iter_data, history):
+            self.assertEqual(i, j)
