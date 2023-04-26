cc1fe4fd96e15d6f336c690c7a84f72d78315ccd
Grzegorz Dynak <grzegorzdynaq@gmail.com>
2023-04-26 22:33:33 +0200

    Add tests with sample data

--- tests/spotifytests/testSpotifyTopListScr.py
+++ tests/spotifytests/testSpotifyTopListScr.py
@@ -1,11 +1,62 @@
 import unittest
-from aplication.app import SpotifyTopListScreen
+from aplication.app import SpotifyTopListScreen, setUp
+import pandas as pd
 
 
 class TestSpotifyTopListScreen(unittest.TestCase):
-    def test_create_top_lists(self):
+    app = setUp()
+    app.screen_manager = app.build()
+
+    def test_create_top_lists_top_tracks(self):
         spotify_top_list_screen = SpotifyTopListScreen()
         spotify_top_list_screen.createTopLists()
         self.assertIsNotNone(spotify_top_list_screen.topTracks)
+
+    def test_create_top_lists_top_artists(self):
+        spotify_top_list_screen = SpotifyTopListScreen()
+        spotify_top_list_screen.createTopLists()
         self.assertIsNotNone(spotify_top_list_screen.topArtists)
+
+    def test_create_top_lists_recommendations(self):
+        spotify_top_list_screen = SpotifyTopListScreen()
+        spotify_top_list_screen.createTopLists()
         self.assertIsNotNone(spotify_top_list_screen.recommendations)
+
+    def test_top_tracks_sample_data(self):
+        spotify_top_list_screen = SpotifyTopListScreen()
+        spotify_top_list_screen.createTopLists()
+
+        with open("./sampledata/spotify_top_tracks.csv") as file:
+            reader = pd.read_csv(file)
+            data = reader.values.tolist()
+
+        fav_songs = spotify_top_list_screen.topTracks
+        iter_data = iter(data)
+        for i, j in zip(iter_data, fav_songs):
+            self.assertEqual(i, j)
+
+    def test_top_artists_sample_data(self):
+        spotify_top_list_screen = SpotifyTopListScreen()
+        spotify_top_list_screen.createTopLists()
+
+        with open("./sampledata/spotify_top_artists.csv") as file:
+            reader = pd.read_csv(file)
+            data = reader.values.tolist()
+
+        fav_songs = spotify_top_list_screen.topTracks
+        iter_data = iter(data)
+        for i, j in zip(iter_data, fav_songs):
+            self.assertEqual(i, j)
+
+    def test_recommendations_sample_data(self):
+        spotify_top_list_screen = SpotifyTopListScreen()
+        spotify_top_list_screen.createTopLists()
+
+        with open("./sampledata/spotify_recommendations.csv") as file:
+            reader = pd.read_csv(file)
+            data = reader.values.tolist()
+
+        fav_songs = spotify_top_list_screen.topTracks
+        iter_data = iter(data)
+        for i, j in zip(iter_data, fav_songs):
+            self.assertEqual(i, j)
