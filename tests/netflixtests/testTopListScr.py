037a60d71a7b37a6bd81b071ecb2b7d060831f08
Grzegorz Dynak <grzegorzdynaq@gmail.com>
2023-04-27 20:17:39 +0200

    Turning tests into smaller ones

--- tests/netflixtests/testTopListScr.py
+++ tests/netflixtests/testTopListScr.py
@@ -6,11 +6,27 @@ class TestNetflixTopListScreen(unittest.TestCase):
     app = setUp()
     app.screen_manager = app.build()
 
-    def test_find_top(self):
+    def test_find_top_actors(self):
         netflix_top_lists_screen = NetflixTopListScreen()
         netflix_top_lists_screen.findTop()
         self.assertIsNotNone(netflix_top_lists_screen.topActors)
+
+    def test_find_top_genres(self):
+        netflix_top_lists_screen = NetflixTopListScreen()
+        netflix_top_lists_screen.findTop()
         self.assertIsNotNone(netflix_top_lists_screen.topGenres)
+
+    def test_find_top_series(self):
+        netflix_top_lists_screen = NetflixTopListScreen()
+        netflix_top_lists_screen.findTop()
         self.assertIsNotNone(netflix_top_lists_screen.topSeries)
+
+    def test_find_most_popular_watched(self):
+        netflix_top_lists_screen = NetflixTopListScreen()
+        netflix_top_lists_screen.findTop()
         self.assertIsNotNone(netflix_top_lists_screen.mostPopularWatched)
+
+    def test_find_least_popular_watched(self):
+        netflix_top_lists_screen = NetflixTopListScreen()
+        netflix_top_lists_screen.findTop()
         self.assertIsNotNone(netflix_top_lists_screen.leastPopularWatched)
