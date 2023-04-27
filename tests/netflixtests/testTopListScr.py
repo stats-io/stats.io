c5eb63f1af58ef35f9d7d70587ab1e152e68e47f
Grzegorz Dynak <grzegorzdynaq@gmail.com>
2023-04-27 20:39:42 +0200

    Add tests with sample data

--- tests/netflixtests/testTopListScr.py
+++ tests/netflixtests/testTopListScr.py
@@ -1,5 +1,6 @@
 import unittest
 from aplication.app import NetflixTopListScreen, setUp
+import pandas as pd
 
 
 class TestNetflixTopListScreen(unittest.TestCase):
@@ -30,3 +31,29 @@ class TestNetflixTopListScreen(unittest.TestCase):
         netflix_top_lists_screen = NetflixTopListScreen()
         netflix_top_lists_screen.findTop()
         self.assertIsNotNone(netflix_top_lists_screen.leastPopularWatched)
+
+    def test_find_most_popular_watched_sample_data(self):
+        netflix_top_lists_screen = NetflixTopListScreen()
+        netflix_top_lists_screen.findTop()
+
+        with open("./sampledata/netflix_most_popular_watched") as file:
+            reader = pd.read_csv(file)
+            data = reader.values.tolist()
+
+        most_popular = netflix_top_lists_screen.mostPopularWatched
+        iter_data = iter(data)
+        for i, j in zip(iter_data, most_popular):
+            self.assertEqual(i, j)
+
+    def test_find_least_popular_watched_sample_data(self):
+        netflix_top_lists_screen = NetflixTopListScreen()
+        netflix_top_lists_screen.findTop()
+
+        with open("./sampledata/netflix_least_popular_watched") as file:
+            reader = pd.read_csv(file)
+            data = reader.values.tolist()
+
+        most_popular = netflix_top_lists_screen.leastPopularWatched
+        iter_data = iter(data)
+        for i, j in zip(iter_data, most_popular):
+            self.assertEqual(i, j)
