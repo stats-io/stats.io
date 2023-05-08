6713300991b4c81dc1b85c8bcb086d3fff6b0612
Sob0r2 <121493914+Sob0r2@users.noreply.github.com>
2023-05-08 16:45:09 +0200

    Create NetflixMainScreen.py

---
+++ app/backend/NetflixMainScreen.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+class NetflixMainScreen:
+
+    def __init__(self,file):
+        self.csvFile = file
+        self.TotalMoviesWatched = self.CountMovies()
+        self.TotalSeriesWatched = self.CountSeries()
+
+    def CountMovies(self):
+        self.DataArray = pd.read_csv(self.csvFile)
+        return len(self.DataArray[self.DataArray['type'] == 'film'])
+
+    def CountSeries(self):
+        self.DataArray = pd.read_csv(self.csvFile)
+        return len(self.DataArray[self.DataArray['type'] == 'series'])
