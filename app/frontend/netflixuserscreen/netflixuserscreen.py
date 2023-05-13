e19e033f40f93858c2039262b067f067750d9d34
gregusio <grzegorzdynaq@gmail.com>
2023-05-14 01:10:32 +0200

    Add basic top lists

--- app/frontend/netflixuserscreen/netflixuserscreen.py
+++ app/frontend/netflixuserscreen/netflixuserscreen.py
@@ -1,12 +1,21 @@
 from kivymd.uix.screen import MDScreen
-from kivymd.uix.list import ThreeLineListItem
+from kivymd.uix.list import OneLineListItem, TwoLineListItem, ThreeLineListItem
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 
+from app.backend.NetflixTopLists import NetflixTopLists
 from app.backend.NetflixCharts import NetflixCharts
 from app.backend.NetflixMainScreen import NetflixMainScreen
 import pandas as pd
 
 import time
+
+
+class CustomOneLineListItem(OneLineListItem):
+    pass
+
+
+class CustomTwoLineListItem(TwoLineListItem):
+    pass
 
 
 class CustomThreeLineListItem(ThreeLineListItem):
@@ -15,6 +24,7 @@ class CustomThreeLineListItem(ThreeLineListItem):
 
 class NetflixUserScreen(MDScreen):
     netflix_main_screen = NetflixMainScreen()
+    netflix_top_lists = NetflixTopLists()
     charts = NetflixCharts()
 
     def generate_charts(self):
@@ -27,6 +37,7 @@ class NetflixUserScreen(MDScreen):
         creator = self.manager.get_screen("netflixuserscreen").ids.boks
         creator.add_widget(FigureCanvasKivyAgg(self.charts.GenresChart()))
         self.generate_history()
+        self.create_top_list()
 
 
     def generate_history(self):
@@ -46,3 +57,45 @@ class NetflixUserScreen(MDScreen):
                     tertiary_text =data_array[row]['Dates'].replace('[', '').replace(']', '').replace('\'', ''),
                 )
             )
+
+    def create_top_list(self):
+        custom_list = self.manager.get_screen("netflixuserscreen").ids.netflixtoplistscreen
+        index = 1
+        for ind1, row1 in self.netflix_top_lists.TopActors.iterrows():
+            list_item = CustomOneLineListItem(
+                text = str(index) + ". " + ind1
+            )
+            index += 1
+            custom_list.add_widget(list_item, 8)
+
+        index = 1
+        for ind1, row1 in self.netflix_top_lists.TopGenres.iterrows():
+            list_item = CustomOneLineListItem(
+                text = str(index) + ". " + ind1
+            )
+            index += 1
+            custom_list.add_widget(list_item, 6)
+
+        index = 1
+        for ind1, row1 in self.netflix_top_lists.TopSeries.iterrows():
+            list_item = CustomOneLineListItem(
+                text = str(index) + ". " + row1[0]
+            )
+            index += 1
+            custom_list.add_widget(list_item, 4)
+
+        index = 1
+        for ind1, row1 in self.netflix_top_lists.MostPopularWatched.iterrows():
+            list_item = CustomOneLineListItem(
+                text = str(index) + ". " + row1[0]
+            )
+            index += 1
+            custom_list.add_widget(list_item, 2)
+
+        index = 1
+        for ind1, row1 in self.netflix_top_lists.TopDayWatched.iterrows():
+            list_item = CustomOneLineListItem(
+                text = str(index) + ". " + str(row1)
+            )
+            index += 1
+            custom_list.add_widget(list_item, 0)
