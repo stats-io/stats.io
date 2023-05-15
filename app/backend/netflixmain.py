2db566e6f6b46daefadda18b93497524d588edc7
Sob0r2 <121493914+Sob0r2@users.noreply.github.com>
2023-05-15 13:29:01 +0200

    Update netflixmain.py

--- app/backend/netflixmain.py
+++ app/backend/netflixmain.py
@@ -3,10 +3,32 @@ import pandas as pd
 
 class NetflixMainScreen:
 
-    def __init__(self, file="app/backend/files/BigFile.csv"):
-        self.csvFile = file
-        self.TotalMoviesWatched = self.CountMovies()
-        self.TotalSeriesWatched = self.CountSeries()
+     def __init__(self,file="app/backend/files/Final_Data.csv"):
+        self.csvFile = self.CSVFile(file)
+        self.TotalMoviesWatched = 0
+        self.TotalSeriesWatched = 0
+
+
+    def CSVFile(self,file):
+        f = "app/backend/files/"
+        test = 1
+        try:
+            df = pd.read_csv(file)
+        except pd.errors.EmptyDataError:
+            test = 0
+        if test == 0:
+            files = [f"{f}/LastSmallData.csv", f"{f}/LastBigData.csv"]
+            latest_file = max(files, key=os.path.getmtime)
+            file = latest_file
+            try:
+                df = pd.read_csv(file)
+            except pd.errors.EmptyDataError:
+                if latest_file == f"{f}/LastBigData.csv":
+                    file = f"{f}/LastSmallData.csv"
+                else:
+                    file = f"{f}/LastBigData.csv"
+        return file
+
 
     def CountMovies(self):
         self.DataArray = pd.read_csv(self.csvFile)
