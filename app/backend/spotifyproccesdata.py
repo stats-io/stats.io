ad3edd604dc6662a601c08c1ce50e1f0f880a558
Grzegorz Dynak <grzegorzdynaq@gmail.com>
2023-05-20 21:39:16 +0200

    Add data processing using spotipy

--- app/backend/spotifyproccesdata.py
+++ app/backend/spotifyproccesdata.py
@@ -1,11 +1,59 @@
 import pandas as pd
 import zipfile
+import csv
 
 
 class SpotifyProcessData:
-
-    def __init__(self,file="app/backend/files/Spotify/my_spotify_data.zip"):
+    def __init__(self, file="app/backend/files/Spotify/my_spotify_data.zip"):
         self.FolderDir = file
+
+    def process_data_from_spotipy(self, sp):
+        self.get_recently_played_tracks(sp)
+        self.get_top_tracks(sp)
+
+    def get_recently_played_tracks(self, sp):
+        results = sp.current_user_recently_played(limit=50)
+
+        with open(
+            "app/backend/files/Spotify/recently_played_tracks.csv",
+            "w",
+            newline="",
+            encoding="utf-8",
+        ) as file:
+            writer = csv.writer(file)
+
+            # Zapisz nagłówki kolumn
+            headers = ["Nr", "Title", "Artists"]
+            writer.writerow(headers)
+
+            # Zapisz dane do pliku
+            for i, item in enumerate(results["items"]):
+                track = item["track"]
+                artists = ", ".join([artist["name"] for artist in track["artists"]])
+                row = [i + 1, track["name"], artists]
+                writer.writerow(row)
+
+    def get_top_tracks(self, sp):
+        results = sp.current_user_top_tracks(limit=50)
+
+        with open(
+            "app/backend/files/Spotify/top_tracks.csv",
+            "w",
+            newline="",
+            encoding="utf-8",
+        ) as file:
+            writer = csv.writer(file)
+
+            # Zapisz nagłówki kolumn
+            headers = ["Nr", "Title", "Artists"]
+            writer.writerow(headers)
+
+            # Zapisz dane do pliku
+            for i, item in enumerate(results["items"]):
+                track = item["name"]
+                artist = item["artists"][0]["name"]
+                row = [i + 1, track, artist]
+                writer.writerow(row)
 
     def ProcessDataFromFile(self):
         json_files = []
