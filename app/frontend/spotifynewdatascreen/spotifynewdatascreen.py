55d1bdc80c270414d197f276ee6120adfcc6c89f
Grzegorz Dynak <grzegorzdynaq@gmail.com>
2023-05-20 21:38:09 +0200

    Add basic start processing data and skip screen

--- app/frontend/spotifynewdatascreen/spotifynewdatascreen.py
+++ app/frontend/spotifynewdatascreen/spotifynewdatascreen.py
@@ -2,5 +2,11 @@ from kivymd.uix.screen import MDScreen
 
 
 class SpotifyNewDataScreen(MDScreen):
+    def start_processing_data(self):
+        self.parent.get_screen("spotifyloadingscreen").start_processing()
+        self.parent.current = "spotifyloadingscreen"
+        self.parent.get_screen("spotifyloadingscreen").start_animation()
+
     def skip_processing_data(self):
-        self.parent.current = "spotifyloginscreen"
+        self.parent.get_screen("spotifyloadingscreen").start_processing()
+        self.parent.current = "spotifyuserscreen"
