2962a0e6399c5b99e21cb5c07dbfbd6f987e1e3b
Grzegorz Dynak <grzegorzdynaq@gmail.com>
2023-05-20 21:37:30 +0200

    Add login to Spotify Api

--- app/frontend/spotifyloginscreen/spotifyloginscreen.py
+++ app/frontend/spotifyloginscreen/spotifyloginscreen.py
@@ -1,6 +1,38 @@
 from kivymd.uix.screen import MDScreen
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from app.frontend.spotifyloginscreen.config import (
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI,
+    SCOPE,
+)
+import os
 
 
 class SpotifyLoginScreen(MDScreen):
     def skip_login(self):
-        self.parent.current = "spotifyuserscreen"
+        if os.path.isfile(".cache"):
+            sp = spotipy.Spotify(
+                auth_manager=SpotifyOAuth(
+                    client_id=CLIENT_ID,
+                    client_secret=CLIENT_SECRET,
+                    redirect_uri=REDIRECT_URI,
+                    scope=SCOPE,
+                )
+            )
+            self.parent.get_screen("spotifyloadingscreen").sp = sp
+            self.parent.current = "spotifynewdatascreen"
+
+    def login(self):
+        sp = spotipy.Spotify(
+            auth_manager=SpotifyOAuth(
+                client_id=CLIENT_ID,
+                client_secret=CLIENT_SECRET,
+                redirect_uri=REDIRECT_URI,
+                scope=SCOPE,
+            )
+        )
+        user = sp.current_user()
+        self.parent.get_screen("spotifyloadingscreen").sp = sp
+        self.parent.current = "spotifynewdatascreen"
