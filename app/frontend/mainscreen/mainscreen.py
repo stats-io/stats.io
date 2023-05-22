677c2216761b567934de14944b30e6a96bb6b525
Grzegorz Dynak <grzegorzdynaq@gmail.com>
2023-05-22 16:26:11 +0200

    Add skip login if user have already logged

--- app/frontend/mainscreen/mainscreen.py
+++ app/frontend/mainscreen/mainscreen.py
@@ -1,5 +1,27 @@
 from kivymd.uix.screen import MDScreen
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from config import (
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI,
+    SCOPE,
+)
 
 
 class MainScreen(MDScreen):
-    pass
+    def login_screen(self):
+        if os.path.isfile(".cache"):
+            sp = spotipy.Spotify(
+                auth_manager=SpotifyOAuth(
+                    client_id=CLIENT_ID,
+                    client_secret=CLIENT_SECRET,
+                    redirect_uri=REDIRECT_URI,
+                    scope=SCOPE,
+                )
+            )
+            self.parent.get_screen("spotifyloadingscreen").sp = sp
+            self.parent.current = "spotifynewdatascreen"
+        else:
+            self.parent.current = "spotifyloginscreen"
