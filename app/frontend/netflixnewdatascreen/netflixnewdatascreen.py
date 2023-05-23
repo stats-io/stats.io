5406919112589e85e17196fb50d3bfb3ea3b611c
Sob0r2 <121493914+Sob0r2@users.noreply.github.com>
2023-05-23 14:07:54 +0200

    making a file manager

--- app/frontend/netflixnewdatascreen/netflixnewdatascreen.py
+++ app/frontend/netflixnewdatascreen/netflixnewdatascreen.py
@@ -1,11 +1,18 @@
 import os
-
+import pandas as pd
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
 from plyer import filechooser
 
+app_folder = os.path.abspath('app/backend/files/Netflix')
+user_file_last = os.path.abspath("app/backend/files/Netflix/LastTestFile.csv")
+user_data = os.path.abspath('app/backend/files/Netflix/test.csv')
+last_data = os.path.abspath('app/backend/files/Netflix/LastData.csv')
 
 class NetflixNewDataScreen(MDScreen):
     __banner_open = False
+    dialog = None
 
     def help_banner_handler(self):
         if not self.__banner_open:
@@ -17,24 +24,60 @@ class NetflixNewDataScreen(MDScreen):
         self.__banner_open = not self.__banner_open
 
     def start_processing_data(self):
-        # [todo] if nothing added don't accept such case
-        file_path = self.parent.get_screen("netflixnewdatascreen").ids.filename.text
-        self.parent.get_screen("netflixloadingscreen").start_processing(file_path)
-        self.parent.current = "netflixloadingscreen"
+        try:
+            df = pd.read_csv(user_data)
+            self.parent.get_screen("netflixloadingscreen").start_processing(self.destination_path)
+            self.parent.current = "netflixloadingscreen"
+        except pd.errors.EmptyDataError:
+            self.dialog = MDDialog(
+                text="Please, add a file to enjoy your data!",
+                buttons=[
+                    MDFlatButton(
+                        text="OK",
+                        theme_text_color="Custom",
+                        text_color="#080808",
+                        on_release=self.close_dialog,
+                    ),
+                ],
+            )
+            self.dialog.open()
+
+    def close_dialog(self,*args):
+        self.dialog.dismiss()
 
     def skip_processing_data(self):
-        # [todo] cannot skip if there is no file saved before
-        self.parent.get_screen("netflixloadingscreen").skip_processing()
-        self.parent.current = "netflixuserscreen"
+        try:
+            df = pd.read_csv(last_data)
+            self.parent.get_screen("netflixloadingscreen").skip_processing()
+            self.parent.current = "netflixuserscreen"
+        except pd.errors.EmptyDataError:
+            self.dialog = MDDialog(
+                text="""This is your first time using of this app, 
+follow the instructions above and add a csv file!""",
+                buttons=[
+                    MDFlatButton(
+                        text="OK",
+                        theme_text_color="Custom",
+                        text_color="#080808",
+                        on_release=self.close_dialog,
+                    ),
+                ],
+            )
+        self.dialog.open()
+
 
     def file_manager_open(self):
         filechooser.open_file(on_selection=self.__handle_selection)
 
     def __handle_selection(self, selection):
         if selection:
-            file_path = os.path.normpath(os.path.abspath(selection[0]))
+            file_path = selection[0]
+            df = pd.read_csv(file_path)
+            self.destination_path = os.path.join(app_folder,'test.csv')
+            df.to_csv(self.destination_path,index=False)
+            df.to_csv(user_file_last,index=False)
             self.parent.get_screen("netflixnewdatascreen").ids.filemanagericon.icon = "check-circle"
             self.parent.get_screen("netflixnewdatascreen").ids.fileadd.text = "Chosen file"
-            self.parent.get_screen("netflixnewdatascreen").ids.filename.text = f"{file_path}"
+            self.parent.get_screen("netflixnewdatascreen").ids.filename.text = f"{selection[0]}"
         else:
             pass
