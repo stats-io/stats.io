ff5ebf1df2a899ac4e4274741e89afd8031fece6
Sob0r2 <121493914+Sob0r2@users.noreply.github.com>
2023-05-23 14:43:47 +0200

    Update netflixloadingscreen.py

--- app/frontend/netflixloadingscreen/netflixloadingscreen.py
+++ app/frontend/netflixloadingscreen/netflixloadingscreen.py
@@ -1,190 +1,45 @@
-import pandas as pd
-import os
+from time import sleep
+from threading import Thread
 
-from kivymd.uix.list import OneLineListItem, TwoLineListItem, ThreeLineListItem
+from kivy.clock import Clock
 from kivymd.uix.screen import MDScreen
-from kivymd.uix.card import MDCard
-from kivymd.uix.dialog import MDDialog
-from kivymd.uix.button import MDFlatButton
 
-from app.backend.netflixcharts import NetflixCharts
-from app.backend.netflixmain import NetflixMainScreen
-from app.backend.netflixtoplists import NetflixTopLists
-from app.backend.tmdbapi import single_movie_search
-from libs.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
-
-user_file = os.path.abspath("app/backend/files/Netflix/test.csv")
-user_file_last = os.path.abspath("app/backend/files/Netflix/LastTestFile.csv")
-
-class CustomOneLineListItem(OneLineListItem):
-    pass
+import app.backend.netflixloading as Loading
 
 
-class CustomTwoLineListItem(TwoLineListItem):
-    pass
+class NetflixLoadingScreen(MDScreen):
 
-
-class CustomThreeLineListItem(ThreeLineListItem):
-    pass
-
-
-class CustomButton(MDCard):
-    __dialog = None
-
-    def close_dialog(self, *args):
-        self.__dialog.dismiss()
-
-    def show_bigger(self, title, date_watched):
-        overview, genres, actors = single_movie_search(title)
-
-        if not self.__dialog:
-            self.__dialog = MDDialog(
-                title=f"{title}\n{date_watched}",
-                text=f"Genres: {genres}\n\nActors: {actors}\n\nOverview: {overview}",
-                md_bg_color="#E0E0E0",
-                buttons=[
-                    MDFlatButton(
-                        text="CLOSE",
-                        theme_text_color="Custom",
-                        text_color="#080808",
-                        md_bg_color="#A7F500",
-                        on_release=self.close_dialog,
-                    )
-                ],
-            )
-        self.__dialog.open()
-
-
-class NetflixUserScreen(MDScreen):
-    def generate_screens(self):
-        self.__generate_main_screen()
-        self.__generate_charts()
-        self.__generate_history("")
-        self.__generate_top_lists()
-
-    def __generate_main_screen(self):
-        netflix_main_screen = NetflixMainScreen()
-        self.manager.get_screen("netflixuserscreen").ids.total_movies.text = str(
-            netflix_main_screen.count_movies()
-        )
-        self.manager.get_screen("netflixuserscreen").ids.total_series.text = str(
-            netflix_main_screen.count_series()
-        )
-
-    def __generate_charts(self):
-        charts = NetflixCharts()
-        charts_screen = self.manager.get_screen("netflixuserscreen").ids
-        charts_screen.genres_chart.add_widget(
-            FigureCanvasKivyAgg(charts.genres_chart())
-        )
-        charts_screen.movies_series_chart.add_widget(
-            FigureCanvasKivyAgg(charts.series_vs_film_chart())
-        )
-        charts_screen.years_chart.add_widget(
-            FigureCanvasKivyAgg(charts.favourite_year())
-        )
-        charts_screen.watch_count_chart.add_widget(
-            FigureCanvasKivyAgg(charts.dates_chart())
-        )
-        charts_screen.time_at_series.add_widget(
-            FigureCanvasKivyAgg(charts.time_at_series())
-        )
-
-    def search_history(self):
-        text = self.manager.get_screen("netflixuserscreen").ids.textfield.text
-        self.__generate_history(text)
-
-    def __generate_history(self, text):
-        try:
-            df = pd.read_csv(user_file)
-        except  pd.errors.EmptyDataError:
-            df = pd.read_csv(user_file_last)
-        if text.strip() == "":
-            data_array = df.to_dict("records")
+    def _update_label(self, *args):
+        if self.__counter < self.__num and self.__loading_screen.finished_loading == 0:
+            self.__counter += 1
+            percent = self.__counter * 100 // self.__num
+            self.manager.get_screen(
+                "netflixloadingscreen"
+            ).ids.loadinglabel.text = f"{percent}%"
         else:
-            data_array = df[df["Title"].str.contains(text, case=False)]
-            data_array = data_array.to_dict("records")
+            self.manager.get_screen(
+                "netflixloadingscreen"
+            ).ids.loadinglabel.text = f"100%"
+            while not self.__loading_screen.finished_loading:
+                sleep(1)
+            self.__timer.cancel()
+            self.skip_processing()
 
-        children = self.manager.get_screen("netflixuserscreen").ids.historylist.children
-        excess_children = children[:-3]
-        for child in excess_children:
-            self.manager.get_screen("netflixuserscreen").ids.historylist.remove_widget(
-                child
-            )
-        self.__generate_history_list(data_array)
+    def _animation_handler(self, *args):
+        self.__loading_screen = Loading.NetflixLoadingScreen()
+        est_time, self.__num = self.__loading_screen.get_estimated_time(self.__file_path)
+        backend_thread = Thread(target=self.__loading_screen.start_processing_data).start()
+        self.manager.get_screen(
+            "netflixloadingscreen"
+        ).ids.estimatedtime.text = f"Estimated time: {round(self.__num * est_time)}s"
+        self.__timer = Clock.schedule_interval(self._update_label, est_time)
 
-    def __generate_history_list(self, data_array):
-        root = self.manager.get_screen("netflixuserscreen").ids.historylist
-        row, possible = 0, 100
-        while row < min(len(data_array), possible):
-            if data_array[row]["Title"].find("_") != -1:
-                possible, row = possible + 1, row + 1
-                continue
-            listelement = CustomButton(size_hint_y=None, height=80)
-            listelement.ids.one_text.text = data_array[row]["Title"]
-            try:
-                listelement.ids.two_text.text = data_array[row]["Date"]
-            except KeyError:
-                listelement.ids.two_text.text = data_array[row]["Start Time"][:10]
-            row += 1
-            root.add_widget(listelement)
-        root.bind(minimum_height=root.setter("height"))
+    def start_processing(self, file_path):
+        self.__counter = 0
+        self.__file_path = file_path
+        self.__timer = Clock
+        self.__timer.schedule_once(self._animation_handler, 0)
 
-    def __generate_top_lists(self):
-        netflix_top_lists = NetflixTopLists()
-        custom_list = self.manager.get_screen(
-            "netflixuserscreen"
-        ).ids.netflixtoplistscreen
-
-        index = 1
-        for person, titles in netflix_top_lists.top_actors.iterrows():
-            list_item = CustomThreeLineListItem(
-                font_style="H6",
-                text_color="#E0E0E0",
-                secondary_text_color="#A7F500",
-                text=f"{index}. {person}",
-                secondary_text=f"{titles[0]} appearances in history",
-                tertiary_text=", ".join(titles[1]),
-            )
-            index += 1
-            custom_list.add_widget(list_item, 8)
-
-        index = 1
-        for genre, number in  netflix_top_lists.top_genres.iterrows():
-            list_item = CustomTwoLineListItem(
-                text=f"{index}. {genre}",
-                secondary_text=f"{number[0]} movies/series"
-            )
-            index += 1
-            custom_list.add_widget(list_item, 6)
-
-        index = 1
-        for serie, hours in netflix_top_lists.top_series.iterrows():
-            time = hours[1].split(':')
-            list_item = CustomTwoLineListItem(
-                text=f"{index}. {hours[0]}",
-                secondary_text=f"Number of episodes: {hours[1]}" if type(hours[1]) == int else f"{time[0]}h {time[1]}m {time[2]}s"
-            )
-            index += 1
-            custom_list.add_widget(list_item, 4)
-
-        index = 1
-        for _, title in netflix_top_lists.most_popular_watched.iterrows():
-            list_item = CustomOneLineListItem(
-                text=f"{index}. {title[0]}"
-            )
-            index += 1
-            custom_list.add_widget(list_item, 2)
-
-        index = 1
-        for date, titles in netflix_top_lists.top_day_watched.iterrows():
-            list_item = CustomThreeLineListItem(
-                font_style="H6",
-                text_color="#E0E0E0",
-                secondary_text_color="#A7F500",
-                text=f"{index}. {date}",
-                secondary_text=f"{titles[0]} titles",
-                tertiary_text=", ".join([f"{item} - {count}" for item, count in sorted(titles[1].items(), key=lambda x: -int(x[1]))]),
-            )
-            index += 1
-            custom_list.add_widget(list_item, 0)
+    def skip_processing(self):
+        self.manager.get_screen("netflixuserscreen").generate_screens()
+        self.manager.current = "netflixuserscreen"
