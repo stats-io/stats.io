51f0ce5c45ecd35a4ee9b5e3b7b3614830ce5f8d
Sob0r2 <121493914+Sob0r2@users.noreply.github.com>
2023-05-23 14:04:30 +0200

    Add files paths

--- app/backend/netflixmain.py
+++ app/backend/netflixmain.py
@@ -1,9 +1,12 @@
 import time
 import pandas as pd
+import os
 
+final_data = os.path.abspath("app/backend/files/Netflix/Final_Data.csv")
+last_data = os.path.abspath("app/backend/files/Netflix/LastData.csv")
 
 class NetflixMainScreen:
-    def __init__(self, file="app/backend/files/Netflix/Final_Data.csv"):
+    def __init__(self, file=final_data):
         self.csv_file = self.read_csv_file(file)
         self.total_movies_watched = 0
         self.total_series_watched = 0
@@ -14,7 +17,7 @@ class NetflixMainScreen:
             return file
         except pd.errors.EmptyDataError:
             pass
-        file = "app/backend/files/Netflix/LastData.csv"
+        file = last_data
         try:
             df = pd.read_csv(file)
             return file
@@ -23,11 +26,11 @@ class NetflixMainScreen:
 
     def count_movies(self):
         time.sleep(1)
-        self.csv_file = self.read_csv_file("app/backend/files/Netflix/Final_Data.csv")
+        self.csv_file = self.read_csv_file(final_data)
         self.data_array = pd.read_csv(self.csv_file)
         return len(self.data_array[self.data_array["type"] == "film"])
 
     def count_series(self):
-        self.csv_file = self.read_csv_file("app/backend/files/Netflix/Final_Data.csv")
+        self.csv_file = self.read_csv_file(final_data)
         self.data_array = pd.read_csv(self.csv_file)
         return len(self.data_array[self.data_array["type"] == "series"])
