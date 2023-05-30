de4fdb3b9aa6b5ec698110d17346fc25e8a15f43
Sob0r2 <121493914+Sob0r2@users.noreply.github.com>
2023-05-30 18:10:10 +0200

    Adding undo module

--- app/frontend/spotifyloginscreen/spotifyloginscreen.py
+++ app/frontend/spotifyloginscreen/spotifyloginscreen.py
@@ -1,6 +1,10 @@
 from kivymd.uix.screen import MDScreen
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from kivy.core.window import Window
+from kivy.config import Config
+
+Config.set('kivy', 'exit_on_escape', '0')
 
 CLIENT_ID = "fb34b1a1fb884d5794990d691867df0f"
 CLIENT_SECRET = "-"
@@ -21,3 +25,13 @@ class SpotifyLoginScreen(MDScreen):
         user = sp.current_user()
         self.parent.get_screen("spotifyloadingscreen").sp = sp
         self.parent.current = "spotifynewdatascreen"
+
+    def on_enter(self):
+        Window.bind(on_keyboard=self.back_click)
+
+    def on_pre_leave(self):
+        Window.unbind(on_keyboard=self.back_click)
+
+    def back_click(self, window, key, keycode, *largs):
+        if key == 27:
+            self.parent.current = "mainscreen"
